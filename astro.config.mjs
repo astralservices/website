470d68c0416602c5990ff146d3ccba0525dc8d1e
Jack Merrill <me@jackmerrill.com>
2022-07-31 16:41:33 -0500

    feat: add prefetch and turbolinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import partytown from "@astrojs/partytown";
+
+import turbolinks from "@astrojs/turbolinks";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astralapp.io",
-  integrations: [react(), tailwind(), sitemap(), partytown()],
+  integrations: [react(), tailwind(), sitemap(), partytown(), turbolinks()],
   vite: {
     define: {
-      "process.env": JSON.stringify(process.env),
-    },
-  },
+      "process.env": JSON.stringify(process.env)
+    }
+  }
 });
