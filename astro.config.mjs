dc7aff72b002c57a1a87b578ffa081132891a076
Jack Merrill <me@jackmerrill.com>
2022-07-31 00:49:52 -0500

    chore: add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [react(), tailwind(), sitemap(), partytown()],
   vite: {
     define: {
-      "process.env": JSON.stringify(process.env),
-    },
-  },
+      "process.env": JSON.stringify(process.env)
+    }
+  }
 });
